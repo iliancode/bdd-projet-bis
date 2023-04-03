2174dc2b4b1698d0f1f5827cdab1611f729a1503
l3miage-benaisil <benaissailian@outlook.fr>
2023-04-03 12:55:54 +0200

    modif valeur enum

--- data/src/main/java/main/java/fr/uga/l3miage/photonum/data/Enum/Reference.java
+++ data/src/main/java/main/java/fr/uga/l3miage/photonum/data/Enum/Reference.java
@@ -2,5 +2,7 @@ package main.java.fr.uga.l3miage.photonum.data.Enum;
 
 
 public enum Reference {
-
+    BRIL10x15,
+    CALA4BRIL,
+    CADA3MAT
 }
--- data/src/main/java/main/java/fr/uga/l3miage/photonum/data/Enum/TailleCadre.java
+++ data/src/main/java/main/java/fr/uga/l3miage/photonum/data/Enum/TailleCadre.java
@@ -3,6 +3,11 @@ package main.java.fr.uga.l3miage.photonum.data.Enum;
 
 
 public enum TailleCadre {
+    A4,
+    A3,
+    A2,
+    A1,
+    A0,
     Petit,
     Moyen,
     Grand
